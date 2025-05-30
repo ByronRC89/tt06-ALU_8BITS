11b9a8bf5112abc3b2b60174a93699822918dc07
BR.c <159856194+ByronRC89@users.noreply.github.com>
2025-05-30 16:39:23 -0600

    Add files via upload

---
+++ src/user_module.v
@@ -0,0 +1,21 @@
+module user_module (
+    input  [7:0] io_in,
+    output [7:0] io_out
+);
+    wire [3:0] A = io_in[7:4];
+    wire [3:0] B = io_in[3:0];
+    wire [2:0] sel = io_in[2:0]; // limitado
+
+    wire [7:0] result;
+    wire cout;
+
+    alu_8bit alu_inst (
+        .A({4'b0000, A}),
+        .B({4'b0000, B}),
+        .sel(sel),
+        .Result(result),
+        .Cout(cout)
+    );
+
+    assign io_out = result;
+endmodule
