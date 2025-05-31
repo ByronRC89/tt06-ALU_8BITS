e938a726e2f81a45e9e12821d2ebaaa8c71cfeaa
BR.c <159856194+ByronRC89@users.noreply.github.com>
2025-05-31 00:21:40 -0600

    Update tt_um_byron.v

--- src/tt_um_byron.v
+++ src/tt_um_byron.v
@@ -2,16 +2,16 @@ module tt_um_byron (
     input  [7:0] io_in,
     output [7:0] io_out
 );
-    wire [3:0] A = io_in[7:4];
-    wire [3:0] B = io_in[3:0];
-    wire [2:0] sel = io_in[2:0]; // limitado
+    wire [2:0] sel = io_in[7:5];  // bits 7-5 para selección
+    wire [2:0] A   = io_in[4:2];  // bits 4-2 para A
+    wire [1:0] B   = io_in[1:0];  // bits 1-0 para B
 
     wire [7:0] result;
     wire cout;
-
+// A y B se extienden a 8 bits
     alu_8bit alu_inst (
-        .A({4'b0000, A}),
-        .B({4'b0000, B}),
+        .A({5'b00000, A}),
+        .B({6'b000000, B}),
         .sel(sel),
         .Result(result),
         .Cout(cout)
